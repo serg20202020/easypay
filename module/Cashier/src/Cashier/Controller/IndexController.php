ad3959dbb58e3b65389b23fadf0d460a103c2b87
itcocoa <3216730556@qq.com>
2015-07-09 13:26:20 +0800

    增加交易生成代码，如果同商家同订单号的交易已存在，则更新该交易的价格和跳转、通知地址等信息。 增加支付方式选择代码。 去掉foo.phtml文件，并把其fooAction更名为setpaymentAction，用于前端通过AJAX设置交易的支付方式到数据库。

--- module/Cashier/src/Cashier/Controller/IndexController.php
+++ module/Cashier/src/Cashier/Controller/IndexController.php
@@ -9,6 +9,7 @@
 
 namespace Cashier\Controller;
 use Zend\Db\TableGateway\TableGateway;
+use Zend\Db\TableGateway\Feature;
 use Zend\Db\ResultSet\ResultSet;
 
 class IndexController extends BaseController
@@ -21,61 +22,94 @@ class IndexController extends BaseController
     {
         if (!$this->verifyRequest()){
             
+            
             $dbAdapter = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
             
-            // Check the parameters if they are valid.
+            
+            /**
+             * Check the parameters if they are valid.
+             */
             $parameters = $this->getRequest()->getQuery();
             
-                // if merchant exsist
-                // if trade|redirect_url|notify_url|sign empty
-                // if price is a floating numberic
-                $exception = new \Exception('Parameters is invalid!');
-                
-                if ( empty(trim($parameters['trade'])) || 
-                     empty(trim($parameters['redirect_url'])) || 
-                     empty(trim($parameters['notify_url'])) || 
-                     empty(trim($parameters['sign'])) ) throw $exception;
-                
-                
-                if ( !is_numeric(trim($parameters['price'])) ) throw $exception;
-                
-                $merchant_validator = new \Zend\Validator\Db\NoRecordExists(
-                    array(
-                        'table'   => 'merchant',
-                        'field'   => 'id',
-                        'adapter' => $dbAdapter
-                    )
-                );
-                if ($merchant_validator->isValid(intval($parameters['merchant']))) throw $exception;
+            // if merchant exsist
+            // if trade|redirect_url|notify_url|sign empty
+            // if price is a floating numberic
+            $exception = new \Exception('Parameters is invalid!');
             
-            // Check if there is an exsist trade
-            $tableGateway = new TableGateway('trade', $dbAdapter);
+            if ( empty(trim($parameters['trade'])) || 
+                 empty(trim($parameters['redirect_url'])) || 
+                 empty(trim($parameters['notify_url'])) || 
+                 empty(trim($parameters['sign'])) ) throw $exception;
+            
+            
+            if ( !is_numeric(trim($parameters['price'])) ) throw $exception;
+            
+            $merchant_validator = new \Zend\Validator\Db\NoRecordExists(
+                array(
+                    'table'   => 'merchant',
+                    'field'   => 'id',
+                    'adapter' => $dbAdapter
+                )
+            );
+            if ($merchant_validator->isValid(intval($parameters['merchant']))) throw $exception;
+            
+                
+                
+                
+            /**
+             * Check if there is an exsist trade.
+             */
+            $tableGateway = new TableGateway('trade', $dbAdapter, new Feature\RowGatewayFeature('id'));
+            
             $rs = $tableGateway->select(array(
                 'merchant_id'=>intval($parameters['merchant']),
                 'merchant_trade_id'=>trim($parameters['trade']),
             ));
+            
             if ($rs instanceof ResultSet){
                 if ($rs->count()){
-                    // Update the exsist trade
+                    
                     $trade = $rs->current();
-                    $trade['price'] = trim($parameters['price']);
-                    $trade->save();
+                    
+                    if ($trade->pay_status){
+                        throw new \Exception('Trade had payed before!');
+                    }else{
+                        
+                        // Update the exsist trade
+                        $trade = $rs->current();
+                        $trade->price = trim($parameters['price']);
+                        $trade->redirect_url = trim($parameters['redirect_url']);
+                        $trade->notify_url = trim($parameters['notify_url']);
+                        $trade->save();
+                        
+                    }
+                    
                 }else{
+                    
                     // Create a new trade
+                    $tableGateway->insert(array(
+                        'merchant_id'=>intval($parameters['merchant']),
+                        'merchant_trade_id'=>trim($parameters['trade']),
+                        'redirect_url'=>trim($parameters['redirect_url']),
+                        'notify_url'=>trim($parameters['notify_url']),
+                        'price'=>trim($parameters['price']),
+                        'pay_status'=>0,
+                        'create_time'=>date('Y-n-j H:i:s',time()),
+                    ));
+                    
                 }
             }else{
                 throw new \Exception('DB Error!');
             }
             
-            
-            
-            
         }
         
-        return array();
+        return array(
+            'price'=>trim($parameters['price'])
+        );
     }
 
-    public function fooAction()
+    public function setpaymentAction()
     {
         // This shows the :controller and :action parameters in default route
         // are working when you browse to /index/index/foo
