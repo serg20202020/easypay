ac7b08f87f450eec85e75625f2b03409485a2ecc
itcocoa <3216730556@qq.com>
2015-09-10 17:11:41 +0800

    增加测试Action（补上漏提交的文件）

--- module/Install/src/Install/Controller/IndexController.php
+++ module/Install/src/Install/Controller/IndexController.php
@@ -14,6 +14,11 @@ use Install\Form\InstallForm;
 use Zend\Db\Adapter\Exception\RuntimeException as AdapterRuntimeException;
 use Install\Model\DB;
 
+use Zend\Db\Sql\Ddl;
+use Zend\Db\Sql\Ddl\Column;
+use Zend\Db\Sql\Ddl\Constraint;
+use Zend\Db\Sql\Sql;
+
 
 class IndexController extends AclController
 {
@@ -58,10 +63,65 @@ class IndexController extends AclController
         return array('form'=>$form);
     }
 
-    public function fooAction()
+    public function testAction()
     {
-        // This shows the :controller and :action parameters in default route
-        // are working when you browse to /index/index/foo
+        $adapter = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
+        $metadata = new \Zend\Db\Metadata\Metadata($adapter);
+        $sql = new Sql($adapter);
+        
+        
+        $test_table_name = 'test';
+        
+        /*
+        $tablenames = $metadata->getTableNames();
+        
+        if (in_array($test_table_name, $tablenames)){
+            // Delete [test]
+            $DropTable = new Ddl\DropTable($test_table_name);
+            $adapter->query(
+                $sql->buildSqlString($DropTable,$adapter),
+                $adapter::QUERY_MODE_EXECUTE
+            );
+        }
+        
+        
+        // Create [test]
+        $CreateTable = new Ddl\CreateTable($test_table_name);
+        $CreateTable->addColumn(new Column\Integer('id',false,null,array('autoincrement'=>true)));
+        $CreateTable->addColumn(new Column\Varchar('text', 100));
+        $CreateTable->addColumn(new Column\Varchar('text2', 100));
+        $CreateTable->addColumn(new Column\Varchar('text3', 100));
+        $CreateTable->addConstraint(new Constraint\PrimaryKey('id'));
+        $CreateTable->addConstraint(new Constraint\UniqueKey(array('text','text3'),'text_UniqueKey'));
+        
+        $adapter->query(
+            $sql->buildSqlString($CreateTable,$adapter),
+            $adapter::QUERY_MODE_EXECUTE
+        );
+        
+        
+        //print_r($metadata->getConstraints($test_table_name));
+        
+        //print_r($metadata->getConstraintKeys('PRIMARY', $test_table_name));
+        //print_r($metadata->getConstraintKeys('text_UniqueKey', $test_table_name));
+        //print_r($metadata->getConstraint('_zf_test_PRIMARY', $test_table_name));
+        //print_r($metadata->getConstraint('_zf_test_text_UniqueKey', $test_table_name));
+        
+        
+        $Constraints = $metadata->getConstraints($test_table_name);
+        print_r($Constraints);
+        
+        
+        $AlterTable = new Ddl\AlterTable($test_table_name);
+        
+        $AlterTable->addConstraint(new Constraint\UniqueKey(array('text','text3'),'text_UniqueKey2'));
+        echo $sql->buildSqlString($AlterTable,$adapter);
+        $adapter->query(
+            $sql->buildSqlString($AlterTable,$adapter),
+            $adapter::QUERY_MODE_EXECUTE
+        );*/
+        
+        
         return array();
     }
 }
