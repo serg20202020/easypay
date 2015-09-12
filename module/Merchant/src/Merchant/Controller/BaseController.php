d9e5ef20a8c4c6324e4f2c141b257f2b706cd03d
itcocoa <3216730556@qq.com>
2015-09-12 13:39:46 +0800

    改进商家后台界面显示

--- module/Merchant/src/Merchant/Controller/BaseController.php
+++ module/Merchant/src/Merchant/Controller/BaseController.php
@@ -33,7 +33,9 @@ class BaseController extends AclController
     
     protected function setChildViews(ViewModel $view_page) {
     
-        $view_menu = new ViewModel();
+        $view_menu = new ViewModel(array(
+            'RouteName'=>$this->getEvent()->getRouteMatch()->getMatchedRouteName()
+        ));
         $view_menu->setTemplate('merchant/common/menu');
         $view_page->addChild($view_menu,'menu');
     
