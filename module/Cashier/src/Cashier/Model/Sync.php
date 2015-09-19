493b71e9ed46ebb7f8d04bb93a8cd6207d2b19ee
itcocoa <3216730556@qq.com>
2015-09-19 17:41:37 +0800

    增加订单状态同步类

---
+++ module/Cashier/src/Cashier/Model/Sync.php
@@ -0,0 +1,18 @@
+<?php
+namespace Cashier\Model;
+
+/**
+ * Sync trade status if pay successfully.
+ * @author kent
+ *
+ */
+class Sync
+{
+
+    function __construct( $merchant_trade_id, $payment_trade_id )
+    {
+        
+    }
+}
+
+?>
